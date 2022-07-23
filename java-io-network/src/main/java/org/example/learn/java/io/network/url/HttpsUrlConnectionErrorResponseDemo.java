c7ba2f1960997556a67a145d3cec3f177c04b4ec
maskedmarker <changjianghui0822@gmail.com>
2022-07-23 20:10:41 +0800

    refactor: getResponseCode()也会触发connection发送请求报文

--- java-io-network/src/main/java/org/example/learn/java/io/network/url/HttpsUrlConnectionErrorResponseDemo.java
+++ java-io-network/src/main/java/org/example/learn/java/io/network/url/HttpsUrlConnectionErrorResponseDemo.java
@@ -5,22 +5,19 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
 
 /**
- * In general, creating a connection to a URL is a multistep process
- * HttpsUrlConnection的一般使用操作步骤
- * stackoverflow有个关于HttpsUrlConnection的非常好的问答,包含了多次请求中cookie的处理,建议参考学习
- * https://stackoverflow.com/questions/2793150/how-to-use-java-net-urlconnection-to-fire-and-handle-http-requests/2793153#2793153
+ * 演示server返回http错误响应
  */
-public class HttpsUrlConnectionBasicDemo {
+public class HttpsUrlConnectionErrorResponseDemo {
     public static void main(String[] args) {
-        String httpsUrl = "https://www.baidu.com";
-        HttpsUrlConnectionBasicDemo demo = new HttpsUrlConnectionBasicDemo();
-        demo.doRequest(httpsUrl, null);
-        demo.doRequest(httpsUrl, "POST");
+        String notFoundHttpsUrl = "https://www.baidu.com//sfasdfa";
+        HttpsUrlConnectionErrorResponseDemo demo = new HttpsUrlConnectionErrorResponseDemo();
+        demo.doRequest(notFoundHttpsUrl, null);
     }
 
     /**
@@ -44,7 +41,6 @@ public class HttpsUrlConnectionBasicDemo {
             // connect()方法调用返回时,已经创建了与server的tcp连接,此时并没有将http协议的request发送给server
             urlConnection.connect();
 
-            // 当getInputStream()方法调用返回时,才会将http request的header和body发送给server
             accessRemoteObject(httpsUrlConnection);
         } catch (IOException e) {
             e.printStackTrace();
@@ -60,35 +56,38 @@ public class HttpsUrlConnectionBasicDemo {
     }
 
     // connection相关参数
-    private void manipulateSetupParameters(HttpsURLConnection urlConnection) {
+    private void manipulateSetupParameters(URLConnection urlConnection) {
         urlConnection.setConnectTimeout(1000);
         urlConnection.setReadTimeout(2000);
         urlConnection.setDoInput(true);
         urlConnection.setDoOutput(true);
-
-        urlConnection.setAllowUserInteraction(false); // 比如要用户输入密码,操作系统会提示
     }
 
     // request相关参数
-    private void manipulateRequestProperties(HttpsURLConnection urlConnection) {
+    private void manipulateRequestProperties(URLConnection urlConnection) {
         urlConnection.setUseCaches(false);
         urlConnection.setIfModifiedSince(1000 * 60);
         urlConnection.addRequestProperty("Accept", "text/*");
     }
 
-    private void accessRemoteObject(HttpsURLConnection httpsUrlConnection) throws IOException {
-        // 当getInputStream()方法调用返回时,才会将http request的header和body发送给server,并等待和解析response.
-        // 能够触发connection发送报文的方法不仅仅只有getInputStream
-        InputStream inputStream = httpsUrlConnection.getInputStream();
-
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8)); //写死编码
+    /**
+     * 当server的http响应码是异常码时,java.net.URLConnection#getInputStream()方法将会抛异常(java.io.FileNotFoundException)
+     * 必须使用java.net.HttpURLConnection#getErrorStream()方法
+     */
+    private void accessRemoteObject(HttpsURLConnection urlConnection) throws IOException {
+        // 当getResponseCode()方法调用返回时,才会将http request的header和body发送给server,并等待和解析response.
+        // 对于response code=404/410的,会new一个FileNotFoundException,等待调用getInputStream()方法时抛出
+        int responseCode = urlConnection.getResponseCode();
+        InputStream inputStream;
+        if (HttpURLConnection.HTTP_OK == responseCode) {
+            inputStream = urlConnection.getInputStream(); // 正常情况下,对应NetworkClient.serverInput
+        } else {
+            inputStream = urlConnection.getErrorStream(); // 底层也是NetworkClient.serverInput
+        }
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
         String readContent;
         while ((readContent = bufferedReader.readLine()) != null) {
             System.out.print(readContent);
         }
-
-        System.out.println();
-        String requestMethod = httpsUrlConnection.getRequestMethod(); // 默认使用的是GET方法
-        System.out.println("requestMethod = " + requestMethod);
     }
 }
