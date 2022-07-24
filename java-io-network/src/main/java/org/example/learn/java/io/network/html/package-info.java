5bf7594ecb721523e44de58529976f810f2038d1
maskedmarker <changjianghui0822@gmail.com>
2022-07-24 09:30:31 +0800

    feature: 新增java解析html文件,占坑

---
+++ java-io-network/src/main/java/org/example/learn/java/io/network/html/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * 这个包是关于java解析html文件的相关的demo.
+ * jdk原生的解析xml的类是非常繁琐的,建议使用第三方提供的jar包
+ *
+ * Parsing and extracting HTML
+ * If all you want is parsing and extracting data from HTML, then better use a HTML parser like Jsoup.
+ * What are the pros/cons of leading HTML parsers in Java (https://stackoverflow.com/questions/3152138/what-are-the-pros-and-cons-of-the-leading-java-html-parsers/3154281#3154281)
+ * How to scan and extract a webpage in Java (https://stackoverflow.com/questions/2835505/how-to-scan-a-website-or-page-for-info-and-bring-it-into-my-program/2835555#2835555)
+ *
+ */
+package org.example.learn.java.io.network.html;
+
