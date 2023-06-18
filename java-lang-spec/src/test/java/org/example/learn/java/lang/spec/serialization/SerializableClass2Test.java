6a84678a6a336e037bc9f72e2c990959969519f7
maskedmarker <changjianghui0822@gmail.com>
2023-06-18 11:41:28 +0800

    feat(java-lang-spec):序列化
    java原生的对象序列化和反序列化

---
+++ java-lang-spec/src/test/java/org/example/learn/java/lang/spec/serialization/SerializableClass2Test.java
@@ -0,0 +1,32 @@
+package org.example.learn.java.lang.spec.serialization;
+
+import org.junit.Test;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class SerializableClass2Test {
+
+    private static final String FILE_NAME_SUFFIX = "ser";
+
+    @Test
+    public void testSerialization() throws IOException {
+        SerializableClass2 person = new SerializableClass2();
+        person.setId(1024);
+        person.setName("zhangsan");
+        System.out.println("person = " + person);
+
+        String objectFileName = SerializableClass2.class.getSimpleName() + FILE_NAME_SUFFIX;
+        ObjectOutputStream oos = new ObjectOutputStream(Files.newOutputStream(Paths.get(objectFileName)));
+        oos.writeObject(person);
+    }
+
+    @Test
+    public void testDeserialization() throws IOException, ClassNotFoundException {
+        String objectFileName = SerializableClass2.class.getSimpleName() + FILE_NAME_SUFFIX;
+        ObjectInputStream ois = new ObjectInputStream(Files.newInputStream(Paths.get(objectFileName)));
+        SerializableClass2 person = (SerializableClass2) ois.readObject();
+        System.out.println("person = " + person);
+    }
+}
