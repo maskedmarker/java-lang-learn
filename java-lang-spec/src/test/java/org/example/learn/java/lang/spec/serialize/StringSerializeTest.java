3ea257b979e70d0594228eb03e1d958bef30ebb6
maskedmarker <changjianghui0822@gmail.com>
2023-08-10 23:28:22 +0800

    feat(java-lang-spec):Java对象序列化

---
+++ java-lang-spec/src/test/java/org/example/learn/java/lang/spec/serialize/StringSerializeTest.java
@@ -0,0 +1,22 @@
+package org.example.learn.java.lang.spec.serialize;
+
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class StringSerializeTest {
+
+    @Test
+    public void test0() throws IOException {
+        String message = "this is a string message";
+        String filePath = "string.serialize";
+        try (ObjectOutputStream oos = new ObjectOutputStream(Files.newOutputStream(Paths.get(filePath)))){
+            oos.writeObject(message);
+        }
+    }
+}
