b6efeb5874054c7eca0d931bb085f2cfeb9827c8
maskedmarker <changjianghui0822@gmail.com>
2024-04-20 14:32:20 +0800

    feat(java-lang-spec):多态

--- java-lang-spec/src/test/java/org/example/learn/java/lang/spec/polymorphism/Ch01PolyTest.java
+++ java-lang-spec/src/test/java/org/example/learn/java/lang/spec/polymorphism/Ch01PolyTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
  * 在 Java 中，多态性是在字节码层面通过虚方法表（Virtual Method Table）来实现的.
  * 虚方法表是 Java 虚拟机在加载类的时候创建的一张表，用于存储类的方法信息，包括方法的地址等。
  *
+ * 方法的地址的确定: 本类虚表中找不到,再找父类的虚表.
  */
 public class Ch01PolyTest {
 
@@ -31,6 +32,10 @@ public class Ch01PolyTest {
         }
     }
 
+    /**
+     * 虽然单独看字节码文件,方法引用是固定的,
+     * 但是在实际执行时,这些方法的引用地址会指向this所属实际类型的虚方法表中对应方法的地址(这些地址在类加载时才确定,并被改写,从而满足多态特性)
+     */
     private static class ClassB extends ClassA {
         public void method1() {
             System.out.println(this.getClass() + " method1 in ClassB");
